48f5e83b30f45d7eaeeaa3b0bf2a9a0bbadb9984
EndsCreed <jonjondm2001@gmail.com>
2023-03-22 07:36:36 -0230

    Sync

--- src/main/java/com/renatusnetwork/managers/MenuManager.java
+++ src/main/java/com/renatusnetwork/managers/MenuManager.java
@@ -7,5 +7,13 @@ import java.util.UUID;
 
 public class MenuManager {
     private HashMap<String, MenuObject> menus = new HashMap<>();
-    private HashMap<UUID, MenuObject> inMenu = 
+    private HashMap<UUID, MenuObject> inMenu = new HashMap<>();
+
+    public MenuManager () {}
+
+    public HashMap<String, MenuObject> getMenus() {
+        return menus;
+    }
+
+    public MenuObject getMenu(String menuName) { return menus.get(menuName); }
 }
